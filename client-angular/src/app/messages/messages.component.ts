98770e8bd9d0fc7de7c9ab9b599c2defabbf826e
savukhin <savukhin@gmail.com>
2022-07-08 14:44:13 +0300

    Added ngfor for messages

--- client-angular/src/app/messages/messages.component.ts
+++ client-angular/src/app/messages/messages.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from 'src/models/message';
+import { User } from 'src/models/user';
+
+class Item {
+  message: Message = new Message();
+  author: User = new User();
+
+  constructor(message:Message, author:User) {
+    this.message = message;
+    this.author = author;
+  }
+}
 
 @Component({
   selector: 'app-messages',
@@ -6,6 +18,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent implements OnInit {
+  items: Item[] = [
+    new Item(
+      new Message("Hi! How are you doing?", new Date()),
+      new User(1, "Kirill Klimonov", false),
+    )
+  ];
 
   constructor() { }
 
