830eeb36a69bba8de1b7a243ee161623eb7157fb
savukhin <savukhin@gmail.com>
2022-07-11 10:27:13 +0300

    Try to pass user variable through router-outlet

--- client-angular/src/app/profile/user-page/user-page.component.ts
+++ client-angular/src/app/profile/user-page/user-page.component.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { AuthService } from 'src/app/services/backend-api/auth.service';
+import { User } from 'src/models/user';
 
 @Component({
     selector: 'app-user-page',
     templateUrl: './user-page.component.html',
-    styleUrls: ['./user-page.component.scss']
+    styleUrls: ['./user-page.component.scss'],
+    providers: [AuthService]
 })
 export class UserPageComponent implements AfterViewInit {
-    profile = {
-        name: "Saveliy Karpukhin",
-        isOnline: true,
-        status: "#notforwar",
-        birthDate: "30.08.2002",
-        city: "Moscow",
-    }
+    user?: User;
+    profile = new User(0,"Saveliy Karpukhin", true, "#notforwar", "30.08.2002", "Moscow")
     editStatus = false
     
     @ViewChild('pleaseDoIt') input: ElementRef<HTMLInputElement> = {} as ElementRef;
 
     showEditStatus() {
-        // document.getElementById('statusInput')?.focus();
         setTimeout(() => {
             this.input.nativeElement.focus();
             this.input.nativeElement.value = this.profile.status;
@@ -30,8 +27,18 @@ export class UserPageComponent implements AfterViewInit {
         this.editStatus = false;
     }
 
-    // constructor() { }
+    constructor(private auth: AuthService, private cdref: ChangeDetectorRef) {
+        console.log("subscription");
+        this.auth.user$.subscribe(
+            user => {
+                console.log("User changed in userpage");
+                
+                this.user = user;
+                this.cdref.detectChanges();
+            }
+        )
+    }
+    
     ngAfterViewInit(): void {
-        console.log(this.input.nativeElement.textContent)
     }
 }
