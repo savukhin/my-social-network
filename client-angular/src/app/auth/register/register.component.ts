4d647ade007bc5cd1eb8aa145c69b03c8850ce63
savukhin <savukhin@gmail.com>
2022-08-17 14:17:32 +0300

    Fix in register.component.ts tabs

--- client-angular/src/app/auth/register/register.component.ts
+++ client-angular/src/app/auth/register/register.component.ts
@@ -4,43 +4,43 @@ import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/backend-api/auth.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
-  providers: [AuthService, FormBuilder]
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss'],
+    providers: [AuthService, FormBuilder]
 })
 export class RegisterComponent implements OnInit {
-  form: FormGroup;
+    form: FormGroup;
 
-  constructor(private authService: AuthService, 
-          private fb:FormBuilder, 
-          private router: Router) {
+    constructor(private authService: AuthService, 
+            private fb:FormBuilder, 
+            private router: Router) {
 
-      this.form = this.fb.group({
-          login: ['',Validators.required],
-          email: ['',Validators.required],
-          password: ['',Validators.required],
-          password2: ['',Validators.required]
-      });
-  }
-
-  ngOnInit(): void {
-  }
-
-  register() {
-    const val = this.form.value;
-
-    if (val.email && val.password) {
-        this.authService.register(val.login, val.email, val.password, val.password2)
-            .subscribe(
-                (response) => {
-                    // console.log("User is logged in");
-                    console.log(response);
-                    // if (response.status == "ok")
-                    //     this.router.navigateByUrl('/');
-                }
-            );
+        this.form = this.fb.group({
+            login: ['',Validators.required],
+            email: ['',Validators.required],
+            password: ['',Validators.required],
+            password2: ['',Validators.required]
+        });
     }
-}
+
+    ngOnInit(): void {
+    }
+
+    register() {
+        const val = this.form.value;
+
+        if (val.email && val.password) {
+            this.authService.register(val.login, val.email, val.password, val.password2)
+                .subscribe(
+                    (response) => {
+                        // console.log("User is logged in");
+                        console.log(response);
+                        // if (response.status == "ok")
+                        //     this.router.navigateByUrl('/');
+                    }
+                );
+        }
+    }
 
 }
